001cc15da2678262ed04235858b7b0d70f8eef48
kehindeoiyanda <kehindeola96@yahoo.com>
2025-03-23 02:22:41 -0400

    created vpc.tf file

--- variable.tf
+++ variable.tf
@@ -13,3 +13,40 @@ variable "environment" {
     description = "environment"
     type        = string
 }
+
+
+# My vpc variables
+variable "vpc_cidr" {
+    description = "cidr block for the vpc"
+    type        = string
+}
+
+variable "public_app_subnet_cidr_az1" {
+    description = "cidr block for the public subnet"
+    type        = string
+}
+
+variable "public_app_subnet_cidr_az2" {
+    description = "cidr block for the public subnet"
+    type        = string
+}
+
+variable "private_app_subnet_cidr_az1" {
+    description = "cidr block for the private subnet"
+    type        = string
+}
+
+variable "private_app_subnet_cidr_az2" {
+    description = "cidr block for the private subnet"
+    type        = string
+}
+
+variable "private_data_subnet_cidr_az1" {
+    description = "cidr block for the private data subnet"
+    type        = string
+}
+
+variable "private_data_subnet_cidr_az2" {
+    description = "cidr block for the private data subnet"
+    type        = string
+}
